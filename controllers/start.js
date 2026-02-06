0f0275328646bd0be8666762fd9b55cfa05ae9d4
(20113253) Ross Bergin <20113253@setu.ie>
2026-02-06 14:46:27 +0000

    Changes now being deployed to GitHub and Render

--- controllers/start.js
+++ controllers/start.js
@@ -6,7 +6,7 @@ import logger from "../utils/logger.js";
 const start = {
     createView(request, response) {
       logger.info("Start page loading!");
-      response.send('Welcome to the Playlist app!');   
+      response.send("Welcome to Ross' Playlist app!");   
     },
   };
   
