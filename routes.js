da3eaadd0e10998e21b5a82361a72302101184ac
(20113253) Ross Bergin <20113253@setu.ie>
2026-02-06 14:26:58 +0000

    First commit

---
+++ routes.js
@@ -0,0 +1,18 @@
+'use strict';
+
+import express from 'express';
+import logger from "./utils/logger.js";
+
+const router = express.Router();
+
+import start from './controllers/start.js';
+import dashboard from './controllers/dashboard.js';
+import about from './controllers/about.js'
+
+router.get('/', start.createView);
+router.get('/dashboard', dashboard.createView);
+router.get('/about', about.createView);
+
+router.get('/error', (request, response) => response.status(404).end('Page not found.'));
+
+export default router;
